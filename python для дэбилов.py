69a93e08e0f6e80c7df194accc418cdb0e5b79d6
Arkhipsik <Arkhipsik@gmail.com>
2018-10-22 12:08:59 +0300

    промежуточные результаты

--- python для дэбилов.py
+++ python для дэбилов.py
@@ -1,6 +1,6 @@
-def hooks(s):
+def scope_balance(input_string):
     counter = 0
-    for sym in s:
+    for sym in input_string:
         if sym == '(':
             counter += 1
         elif sym == ')':
@@ -10,3 +10,38 @@ def hooks(s):
     if counter != 0:
         return False
     return True
+
+
+def sum_operation(input_list):
+    input_list[0] = int(input_list[0])
+    for index in range(0, len(input_list) - 2, 2):
+        input_list[index + 2] = int(input_list[index + 2])
+        if input_list[index + 1] == '+':
+            input_list[index + 2] += input_list[index]
+        elif input_list[index + 1] == '-':
+            input_list[index + 2] -= input_list[index]
+        else:
+            raise SyntaxError('')
+    return input_list[len(input_list) - 1]
+
+
+def multiply_operation(input_list):
+    input_list[0] = int(input_list[0])
+    for index in range(0, len(input_list) - 2, 2):
+        input_list[index + 2] = int(input_list[index + 2])
+        if input_list[index + 1] == '*':
+            input_list[index + 2] *= input_list[index]
+        elif input_list[index + 1] == '%':
+            input_list[index + 2] = input_list[index] % input_list[index + 2]
+        elif input_list[index + 1] == '/':
+            input_list[index + 2] = input_list[index] / input_list[index + 2]
+        elif input_list[index + 1] == '//':
+            input_list[index + 2] = input_list[index] // input_list[index + 2]
+        else:
+            raise SyntaxError('')
+    return input_list[len(input_list) - 1]
+
+
+s = str(input())
+s = s.split(' ')
+print (multiply_operation(s))
