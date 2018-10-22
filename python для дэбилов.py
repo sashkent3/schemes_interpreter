7e784c78e6c7f5bb197a9733fd688356fa51d9e7
Arkhipsik <Arkhipsik@gmail.com>
2018-10-22 11:28:08 +0300

    Скобочный баланс

--- python для дэбилов.py
+++ python для дэбилов.py
@@ -1,1 +1,12 @@
-# новый текст
+def hooks(s):
+    counter = 0
+    for sym in s:
+        if sym == '(':
+            counter += 1
+        elif sym == ')':
+            counter -= 1
+        if counter < 0:
+            return False
+    if counter != 0:
+        return False
+    return True
