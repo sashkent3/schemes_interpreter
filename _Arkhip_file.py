e6e1df13f0601a7cd42a259c8526373477376d07
Arkhip <Arkhipsik@gmail.com>
2018-10-26 23:16:14 +0300

    k

--- _Arkhip_file.py
+++ _Arkhip_file.py
@@ -41,13 +41,29 @@ def multiply_operation(input_list):
             raise SyntaxError('')
     return input_list[len(input_list) - 1]
 
+
 def organisation(input_list):
-    for index in range(1, len(input_list) - 3, 2):
+    # print(input_list, len(input_list), 'organisation')
+    if input_list[0] == '+':
+        input_list = input_list[1:]
+    elif input_list[0] == '-':
+        input_list[1] = '-' + input_list[1]
+        input_list = input_list[1:]
+
+    if len(input_list) == 1:
+        return int(input_list[0])
+
+    for index in range(len(input_list) - 2, 0, - 2):
+        # print(index, input_list[index])
+        # слева направо
         if input_list[index] == '+':
-            organisation()
+            return organisation(input_list[index + 1:]) + organisation(input_list[:index])
         elif input_list[index] == '-':
-#             слева направо
+            return - organisation(input_list[index + 1:]) + organisation(input_list[:index])
+
+    return multiply_operation(input_list)
+
 
 s = str(input())
 s = s.split(' ')
-print (multiply_operation(s))
+print(organisation(s))
