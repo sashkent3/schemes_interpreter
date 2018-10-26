3589f2574f04f8dc5f565852f4f1f6de5baa6658
Arkhip <Arkhipsik@gmail.com>
2018-10-26 23:17:51 +0300

    k

--- _Arkhip_file.py
+++ _Arkhip_file.py
@@ -12,19 +12,6 @@ def scope_balance(input_string):
     return True
 
 
-def sum_operation(input_list):
-    input_list[0] = int(input_list[0])
-    for index in range(0, len(input_list) - 2, 2):
-        input_list[index + 2] = int(input_list[index + 2])
-        if input_list[index + 1] == '+':
-            input_list[index + 2] += input_list[index]
-        elif input_list[index + 1] == '-':
-            input_list[index + 2] -= input_list[index]
-        else:
-            raise SyntaxError('')
-    return input_list[len(input_list) - 1]
-
-
 def multiply_operation(input_list):
     input_list[0] = int(input_list[0])
     for index in range(0, len(input_list) - 2, 2):
